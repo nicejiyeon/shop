64d4a73f24af8b8e4bfd3621dd1f96001d7ff091
jiyeon <jiyeon610@naver.com>
2024-06-07 15:57:30 +0900

    JPA 데이터 입출력 방법 - repository

--- src/main/java/com/apple/shop/ItemController.java
+++ src/main/java/com/apple/shop/ItemController.java
@@ -1,5 +1,6 @@
 package com.apple.shop;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
@@ -7,9 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemRepository itemRepository;
+
     @GetMapping("/list")
     String list(Model model) {
+
+        var result = itemRepository.findAll();
+        System.out.println(result.get(0).title);
+
         model.addAttribute("name", "kildong");
         return "list.html";
 
---
+++ src/main/java/com/apple/shop/ItemRepository.java
@@ -0,0 +1,12 @@
+package com.apple.shop;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> { //Item:테이블, Long:Id Type
+
+    //JPA로 데이터 입출력 하려면
+    //1. repository 만들기
+    //2. 원하는 클래스에 repository 등록
+    //3. repository.입출력문법() 쓰기
+
+}
