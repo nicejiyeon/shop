cf72299a5e69a5dcd1c829743248c4e1ae51a442
jiyeon <jiyeon610@naver.com>
2024-06-11 18:08:16 +0900

    spring @Model 객체

--- src/main/java/com/apple/shop/ItemController.java
+++ src/main/java/com/apple/shop/ItemController.java
@@ -16,8 +16,11 @@ public class ItemController {
     @GetMapping("/list")
     String list(Model model) {
 
+        //Model 객체는 Controller 에서 생성된 데이터를 담아 View 로 전달할 때 사용하는 객체이다.
+        //addAttribute("key", "value") 메서드를 이용해 view에 전달할 데이터를 key, value형식으로 전달할 수 있다
+
         var result = itemRepository.findAll();
-        model.addAttribute("items", result);
+        model.addAttribute("items", result); // list.html(뷰)에서 items 사용
 
         var a = new Item();
         System.out.println(a.toString());
